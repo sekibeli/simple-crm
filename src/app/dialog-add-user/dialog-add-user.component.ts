7f28a1f625c3201e0b1bd9d678d717cff8847989
Julia Georgiew <java2011@me.com>
2023-07-14 21:34:27 +0200

    components and dialog

---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { User } from 'src/models/user.class';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  templateUrl: './dialog-add-user.component.html',
+  styleUrls: ['./dialog-add-user.component.scss']
+})
+export class DialogAddUserComponent {
+  user = new User;
+
+  onNoClick(){}
+}
